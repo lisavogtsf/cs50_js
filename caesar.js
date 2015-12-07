407bd025cae59a33527970d13bd4d0a7762c2d94
Lisa Vogt <lisavogt.sf@gmail.com>
2015-12-07 10:41:04 -0800

    adds specs for caesar's cypher

---
+++ caesar.js
@@ -0,0 +1,10 @@
+/*
+ * caesar.js for CS50 pset2
+ * Lisa Vogt 2015
+ * - from command line take in an integer key 'k'
+ * - prompt the user for a string of plaintext to encode
+ * - using "Caesar's cypher" with key, rotate all letters
+ *  -- maintain case
+ *  -- if not a letter do not change
+ * - print cyphertext
+ */
