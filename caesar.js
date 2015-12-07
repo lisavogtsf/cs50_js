708b8a70ea6f25285bb3f4368c33ca53d18c0495
Lisa Vogt <lisavogt.sf@gmail.com>
2015-12-07 10:41:49 -0800

    corrects spelling of cipher

--- caesar.js
+++ caesar.js
@@ -3,8 +3,8 @@
  * Lisa Vogt 2015
  * - from command line take in an integer key 'k'
  * - prompt the user for a string of plaintext to encode
- * - using "Caesar's cypher" with key, rotate all letters
+ * - using "Caesar's cipher" with key, rotate all letters
  *  -- maintain case
  *  -- if not a letter do not change
- * - print cyphertext
+ * - print ciphertext
  */
