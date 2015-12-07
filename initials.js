6583b80962957d32f09679c4f036f7d7d5c45d8b
Lisa Vogt <lisavogt.sf@gmail.com>
2015-12-07 10:18:51 -0800

    adds initials function from problem set 2

---
+++ initials.js
@@ -0,0 +1,25 @@
+/*
+ * initials.js for CS50 pset2
+ * Lisa Vogt 2015
+ * - take in name (2+ names with spaces, no special characters, any case)
+ * - print initials (uppercase, two letters, newline)
+ */
+
+var initials = function () {
+  // get user input, can assume it's safe
+  var fullname = window.prompt();
+  // break full name into array of individual name stirngs
+  var nameArray = fullname.split(' ');
+  var result = "";
+
+  // iterate over name array, print first letter of each name
+  // in upper case
+  for (var i = 0; i < nameArray.length; i++) {
+    result += nameArray[i].toUpperCase()[0];
+  }
+  // console.log will automatically print a newline
+  // that's why we need to concatenate the results into a string
+  console.log(result);
+};
+initials();
+
